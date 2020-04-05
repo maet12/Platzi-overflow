021062404342f71ad0dec8605e198e6790563f9d
Marco Elizalde <m.elizalde.torres@gmail.com>
2020-04-04 19:11:53 -0600

    Sirviendo archivos estaticos

--- index.js
+++ index.js
@@ -1,30 +1,51 @@
 'use strict'
 
 const hapi = require('hapi');
+const inert = require('inert');
+const path = require('path')
 
 const server = hapi.server({
     port: process.env.PORT || 3000,
-    host: 'localhost'
+    host: 'localhost',
+    routes: {
+        files: {
+            relativeTo: path.join(__dirname, 'public')
+        }
+    }
 })
 
-async function init (){
-    server.route({
-        method: 'GET',
-        path: '/',
-        handler: (req, h) =>{
-            return 'Hola mundo!'
-        }
-    });
+async function init() {
 
     try {
-        await server.start()
+        await server.register(inert);
+
+        server.route({
+            method: 'GET',
+            path: '/home',
+            handler: (req, h) => {
+                return h.file('index.html')
+            }
+        });
+
+        server.route({
+            method: 'GET',
+            path: '/{param*}',
+            handler: {
+                directory: {
+                    path: '.',
+                    index: ['indez.html']
+                }
+            }
+        });
+
+        await server.start();
     } catch (error) {
         console.error(error)
         process(1);
     }
 
     console.log(`Servidor lanzado en: ${server.info.uri}`);
-    
+
 }
 
 init();
