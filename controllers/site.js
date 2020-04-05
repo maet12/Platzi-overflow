50a2e0ec3a9d9770c3035db26dff9b3e6db73bac
Marco Elizalde <m.elizalde.torres@gmail.com>
2020-04-04 19:49:32 -0600

    Restructurando las rutas

---
+++ controllers/site.js
@@ -0,0 +1,13 @@
+'use strict'
+
+function home(req, h) {
+    return h.view('index', { title: 'Home' })
+}
+
+function register(req, h)  {
+    return h.view('register', { title: 'Registro' })
+}
+
+module.exports = {
+    home, register
+}
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const hapi = require('hapi');
 const handlerbasr = require('handlebars')
 const vision = require('vision')
 const inert = require('inert');
+const routes = require('./routes')
 const path = require('path')
 
 const server = hapi.server({
@@ -32,45 +33,7 @@ async function init() {
             layoutPath: 'views'
         })
 
-        server.route({
-            method: 'GET',
-            path: '/',
-            handler: (req, h) => {
-                return h.view('index', { title: 'Home' })
-            }
-        });
-
-        server.route({
-            method: 'GET',
-            path: '/register',
-            handler: (req, h) => {
-                return h.view('register', { title: 'Registro' })
-            }
-        });
-
-        
-        server.route({
-            method: 'POST',
-            path: '/create-user',
-            handler: (req, h) => {
-                console.log(req.payload);
-                
-                return 'Usuario creado'
-            }
-        });
-
-
-        server.route({
-            method: 'GET',
-            path: '/{param*}',
-            handler: {
-                directory: {
-                    path: '.',
-                    index: ['indez.html']
-                }
-            }
-        });
-
+        server.route(routes)
         await server.start();
     } catch (error) {
         console.error(error)
